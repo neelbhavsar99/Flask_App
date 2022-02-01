3fc59c1a1b232496b7eef9254cfed9914a103d51
Neel Bhavsar <neel.bhavsar@ryerson.ca>
2022-01-31 22:53:10 -0500

    bugfix 1.1.1

--- APITester.py
+++ APITester.py
@@ -1,5 +1,5 @@
-import requests
+#import requests
 
-response = requests.get("http://127.0.0.1:5000/pi_output")
-print(response.json())
-print(response.text)
+#response = requests.get("http://127.0.0.1:5000/pi_output")
+#print(response.json())
+#print(response.text)
